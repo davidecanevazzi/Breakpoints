90d8f6980eadfae4eb7643e0be811b998da94bce
davidecanevazzi <50942807+davidecanevazzi@users.noreply.github.com>
2023-01-18 15:28:46 +0100

    Update Mit_breaks.py

--- Mit_breaks.py
+++ Mit_breaks.py
@@ -1,9 +1,9 @@
-#before running cat -n file_name-paf | sort -uk2 | sort -n | cut -f2-
+#before running cat -n filename.paf | sort -uk2 | sort -n | cut -f2-
 import re
 if __name__ == '__main__':
     save=[]
     paf=[]
-    with open("NUMTs_HNO-002-07_uniq_nosort.paf") as br:
+    with open("NUMTs_URO-004-02_uniq.paf") as br:
         for i in br:
             paf.append(i)
 
@@ -15,7 +15,12 @@ if __name__ == '__main__':
                 if (line_curr[5]== 'chrM' and line_prev[5]!='chrM'): #here I check if one signle k-mers is splitted
                     if (line_curr[0][:-3]==line_prev[0][:-3]):
                         if line_prev[4] == '-':
-                            print(line_curr[5], "   ", line_curr[7])
+                            print(line_curr[7])
                         if line_prev[4] == '+':
-                            print(line_curr[5], "   ",line_curr[8])
-
+                            print(line_curr[8])
+                if (line_curr[5]== 'chrM' and line_next[5]!='chrM'): #here I check if one signle k-mers is splitted
+                    if (line_curr[0][:-3]==line_prev[0][:-3]):
+                        if line_prev[4] == '-':
+                            print(line_curr[7])
+                        if line_prev[4] == '+':
+                            print(line_curr[8])
